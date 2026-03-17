df8900d0243cbe636b21a14f68a31135dc8cb0c1
1saifwael-lang <1saifwael@gmail.com>
2026-03-17 11:21:28 +0200

    Create  otp.routes.js

---
+++ otp.routes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { sendOTPController } from "./otp.controller.js";
+const router = Router();
+router.post("/send-otp", sendOTPController);
+export default router;
+ 
